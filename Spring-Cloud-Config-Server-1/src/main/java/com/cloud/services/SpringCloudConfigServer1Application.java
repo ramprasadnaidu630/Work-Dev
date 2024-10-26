7a26d993b211c535a32b8b671b34fcd08fcec8e0
ramprasadnaidu630 <ramprasadnaidu630@gmail.com>
2024-10-26 13:31:52 +0530

    Update SpringCloudConfigServer1Application.java

--- Spring-Cloud-Config-Server-1/src/main/java/com/cloud/services/SpringCloudConfigServer1Application.java
+++ Spring-Cloud-Config-Server-1/src/main/java/com/cloud/services/SpringCloudConfigServer1Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServer1Application {
-
+// RAMPRASAD
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServer1Application.class, args);
 	}
